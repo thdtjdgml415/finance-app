9ca43b457dc1e8497d97a2a672e17a344448a566
SONG <thdtjdgml415@naver.com>
2024-05-12 23:42:05 +0900

    [feat] drizzle, neon postgresql 연동 - drizzle generate, migrate, studio 연동함 - neon 연동

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,6 +1,6 @@
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <div>dashboard Page</div>;
+  return <div>Dashboard Page</div>;
   // return <UserButton afterSignOutUrl="/" />;
 }
